736681cfb5b08857528c618c787a4507df8e275b
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-09-08 14:23:49 +0500

    w

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     const { data } = useContext(FormContext);
     return (
         <header className='text-center py-5 bg-[#1D1D20]'>
-            <div className='md:max-w-[90%] mx-auto max-w-[96%]'>
+            <div className='max-w-[90%] mx-auto'>
                 <div className='flex justify-between items-center'>
                     <h1 className='md:text-xl text-white'>AS Resume Builder</h1>
                     <h2 className='text-white md:text-lg font-semibold'>Welcome {data.name ? data.name + ' ' + data.family : 'User'}</h2>
