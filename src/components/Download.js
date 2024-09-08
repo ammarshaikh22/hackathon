93dbdf7205fc7e7d4ce42ece7809087977342aba
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-09-08 13:49:13 +0500

    d

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,21 +1,16 @@
+import html2pdf from 'html2pdf.js';
 import dynamic from 'next/dynamic';
-const html2pdf = dynamic(() => import('html2pdf.js'), { ssr: false })
+const DynamicHtml2Pdf = dynamic(() => import('html2pdf.js'), { ssr: false });
 export const handleDownload = () => {
-    if (typeof window !== 'undefined') { // Ensure this runs in a browser context
-        const element = document.getElementById('cv-template');
-        if (element) {
-            const opt = {
-                margin: 0,
-                filename: 'YourCv.pdf',
-                image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-            };
-            html2pdf().set(opt).from(element).save();
-        } else {
-            console.error('Element with id "cv-template" not found');
-        }
-    } else {
-        console.error('This function is being called in a non-browser environment');
-    }
+    DynamicHtml2Pdf();
+    const element = document.getElementById('cv-template');
+    const opt = {
+        margin: 0,
+        filename: 'YourCv.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+    };
+    html2pdf().set(opt).from(element).save();
 };
+
